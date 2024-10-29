752ae35eb1339fc04690da6ff48de6ba3302b4a8
valad47 <valad.racz@gmail.com>
2024-10-30 00:37:57 +0200

    Add gdb debugger info flag

--- nob.c
+++ nob.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
   NOB_GO_REBUILD_URSELF(argc, argv);
   Nob_Cmd cmd = {0};
-  nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-o", "vl", "main.c");
+  nob_cmd_append(&cmd, "cc", "-ggdb", "-Wall", "-Wextra", "-o", "vl", "main.c");
   if (!nob_cmd_run_sync(cmd)) return 1;
   return 0;
 }
