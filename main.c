84031282d623d48b808c31e488e51845015d50d3
valad47 <valad.racz@gmail.com>
2024-10-30 00:25:14 +0200

    Add internal stack

--- main.c
+++ main.c
@@ -1,12 +1,46 @@
+#define NOB_IMPLEMENTATION
+#include "nob.h"
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
 
+#define STACK_BASE 8
+
 typedef enum Flags {
   ASM_ONLY = 0b1,
 } Flags;
 
+typedef struct Stack {
+  void **data_array;
+  int sp;
+  int stack_size;
+} Stack;
+
+void stack_push(Stack *stack, void* data) {
+  stack->sp++;
+  if (stack->sp >= stack->stack_size) {
+    stack->stack_size += STACK_BASE;
+    stack->data_array = realloc(stack->data_array, sizeof(void*) * stack->stack_size);
+  }
+  stack->data_array[stack->sp-1] = data;
+}
+
+void *stack_pop(Stack *stack) {
+  if (stack == NULL) {
+    fprintf(stderr, "stack pointer is NULL");
+    exit(1);
+  }
+  if (stack->sp == 0) {
+    return NULL;
+  }
+  stack->sp--;
+  void *ret_val = stack->data_array[stack->sp];
+  stack->data_array[stack->sp] = NULL;
+  return ret_val;
+}
+  
 int main(int argc, char **argv) {
   Flags compile_flags = 0;
   for (int i = 1; i < argc; i++) {
@@ -22,5 +56,8 @@ int main(int argc, char **argv) {
 	  exit(1);
 	}
       }
+    if(nob_file_exists(arg)) {
+      
+    }
   }
 }
